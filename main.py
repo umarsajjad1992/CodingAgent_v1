c580047b9ce2abbd480035b90a15a7300191c003
Umar Sajjad <umar.sajjad8@gmail.com>
2026-04-13 06:25:32 +0400

    create a list to remember conversation between user and llm for context awareness

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     load_dotenv()
@@ -9,14 +10,18 @@ def main():
 
     if len(sys.argv) < 2:
         print("I need a prompt as an argument. Usage: uv run main.py \"<prompt>\"")
-        return
+        sys.exit(1)
     prompt = sys.argv[1]
     
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=prompt)])
+    ]
+
     client = genai.Client(api_key=api_key)
 
     response = client.models.generate_content(
         model="gemini-2.5-flash-lite", 
-        contents=f"""{prompt}"""
+        contents=messages
     )
 
     print(response.text)
