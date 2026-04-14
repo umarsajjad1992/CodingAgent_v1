54b2ef6a5e545129dce9727526f82d67bb94ba2d
Umar Sajjad <umar.sajjad8@gmail.com>
2026-04-14 15:04:43 +0400

    Added a file to retrieve file information and metadata

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,6 +1,6 @@
 import os
 
-def get_files_info(working_dir, dir="."):
+def get_files_info(working_dir, dir=None):
     """
     Get information about files in the specified directory.
 
@@ -11,19 +11,14 @@ def get_files_info(working_dir, dir="."):
     Returns:
         list: A list of file information dictionaries.
     """
+    working_dir_abs = os.path.abspath(working_dir)
     dir_path = os.path.join(working_dir, dir)
     target_dir = os.path.normpath(dir_path)
     file_info_list = []
 
-    for root, dirs, files in os.walk(dir_path):
-        for filename in files:
-            file_path = os.path.join(root, filename)
-            file_info = {
-                "name": filename,
-                "path": file_path,
-                "size": os.path.getsize(file_path),
-                "modified": os.path.getmtime(file_path),
-            }
-            file_info_list.append(file_info)
+    # Will be True or False
+    valid_target_dir = os.path.commonpath([working_dir_abs, target_dir]) == working_dir_abs
 
+    if not valid_target_dir:
+        raise ValueError(f"Invalid directory: {dir}. Directory must be within the working directory.")
     return file_info_list
